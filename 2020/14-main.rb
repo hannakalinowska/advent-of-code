9c3568408551e09e68511e834f7da4cb7a9f6272
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2020-12-14 21:10:52 +0000

    2020 day 14 part 2

--- 2020/14-main.rb
+++ 2020/14-main.rb
@@ -2,10 +2,10 @@
 
 inputs = File.read('14-input.txt').split("\n")
 #inputs = [
-#  'mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X',
-#  'mem[8] = 11',
-#  'mem[7] = 101',
-#  'mem[8] = 0',
+#  'mask = 000000000000000000000000000000X1001X',
+#  'mem[42] = 100',
+#  'mask = 00000000000000000000000000000000X0XX',
+#  'mem[26] = 1',
 #]
 
 mask = nil
@@ -13,13 +13,25 @@ mem = {}
 
 def masked(value, mask)
   -1.downto(-mask.length).reduce([]) {|acc, i|
-    character = (mask[i] == 'X' ? (value[i] || 0) : mask[i])
+    character = (mask[i] == '0' ? (value[i] || 0) : mask[i])
+    #puts "#{value[i]} | #{mask[i]} => #{character}"
     acc << character
     acc
   }
     .reverse
     .join
-    .to_i(2)
+end
+
+def permutations(id)
+  return id if id !~ /X/
+
+  one = id.sub('X', '0')
+  two = id.sub('X', '1')
+
+  [
+    permutations(one),
+    permutations(two),
+  ]
 end
 
 inputs.each do |line|
@@ -27,10 +39,14 @@ inputs.each do |line|
     mask = $1.split('')
   else
     line =~ /mem\[(\d+)\] = (\d+)/
-    id = $1
-    value = $2.to_i.to_s(2).split('')
+    id = $1.to_i.to_s(2).split('')
+    value = $2.to_i
 
-    mem[id] = masked(value, mask)
+    addresses = permutations(masked(id, mask)).flatten
+    addresses.each do |address|
+      #puts "#{address.to_i(2)} [#{address.to_i}] => #{value}"
+      mem[address] = value
+    end
   end
 end
 
