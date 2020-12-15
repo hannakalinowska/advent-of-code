8da5738f8d5f8a52f4c0b806e5ed28edb1fee97c
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2020-12-15 10:53:18 +0000

    Day 15 part 2

--- 2020/15-main.rb
+++ 2020/15-main.rb
@@ -1,21 +1,34 @@
 #! /usr/bin/env ruby
 
 inputs = [16,1,0,18,12,14,19]
-#inputs = [3,1,2]
+#inputs = [1,2,3] # => 27
+#inputs = [3,1,2] # => 1836
+#inputs = [0,3,6] # => [0, 3, 6, 0, 3, 3, 1, 0, 4, 0] # => 436
 
 last_number = inputs.pop
+next_number = nil
+
+numbers = inputs.reduce({}) {|acc, n|
+  acc[n] = inputs.rindex(n) + 1
+  acc
+}
+iteration = inputs.length + 1
 
 loop do
-  if inputs.include?(last_number)
-    next_number = inputs.length - inputs.rindex(last_number)
+  if numbers[last_number]
+    next_number = iteration - numbers[last_number]
   else
     next_number = 0
   end
 
-  inputs << last_number
+  puts iteration if iteration % 1_000_000 == 0
+
+  if iteration >= 30000000
+    puts last_number
+    exit
+  end
+
+  numbers[last_number] = iteration
   last_number = next_number
-
-  break if inputs.length == 2020
+  iteration += 1
 end
-
-puts inputs.last
