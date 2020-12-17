502e64bbf97346cf3a7a264106550f2ed9116a73
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2020-12-17 08:04:59 +0000

    Day 17 part 2.
    I take back what I said yesterday. NOW I win the prize for horrible
    code.

--- 2020/17-main.rb
+++ 2020/17-main.rb
@@ -2,12 +2,12 @@
 
 inputs = File.read('17-input.txt').split
 
-cubes = {0 => {}}
+cubes = {0 => {0 => {}}}
 
 inputs.each.with_index do |row, i|
-  cubes[0][i] ||= {}
+  cubes[0][0][i] ||= {}
   row.split('').each.with_index do |cube, j|
-    cubes[0][i][j] = cube
+    cubes[0][0][i][j] = cube
   end
 end
 
@@ -20,18 +20,23 @@ def update_dimensions(cubes)
 
   $max_z = cubes.values.first.values.first.keys.max
   $min_z = cubes.values.first.values.first.keys.min
+
+  $max_foo = cubes.values.first.values.first.values.first.keys.max
+  $min_foo = cubes.values.first.values.first.values.first.keys.min
 end
 
-def count_active(i, j, k, cubes)
+def count_active(i, j, k, f, cubes)
   count = 0
   (i-1 .. i+1).each do |x|
     (j-1 .. j+1).each do |y|
       (k-1 .. k+1).each do |z|
-        next if x==i && y==j && z==k
+        (f-1 .. f+1).each do |foo|
+          next if x==i && y==j && z==k && foo == f
 
-        cube = cubes.fetch(x, {}).fetch(y, {}).fetch(z, '.')
-        if cube == '#'
-          count += 1
+          cube = cubes.fetch(x, {}).fetch(y, {}).fetch(z, {}).fetch(foo, '.')
+          if cube == '#'
+            count += 1
+          end
         end
       end
     end
@@ -48,15 +53,19 @@ def step(cubes)
       new_cubes[i][j] ||= {}
       row = plane[j] || {}
       ($min_z - 1 .. $max_z + 1).each do |k|
-        active = count_active(i, j, k, cubes)
-        cube = row[k]
+        new_cubes[i][j][k] ||= {}
+        foobar = row[k] || {}
+        ($min_foo - 1 .. $max_foo + 1).each do |f|
+          active = count_active(i, j, k, f, cubes)
+          cube = foobar[f]
 
-        if active == 3
-          new_cubes[i][j][k] = '#'
-        elsif active == 2 && cube == '#'
-          new_cubes[i][j][k] = '#'
-        else
-          new_cubes[i][j][k] = '.'
+          if active == 3
+            new_cubes[i][j][k][f] = '#'
+          elsif active == 2 && cube == '#'
+            new_cubes[i][j][k][f] = '#'
+          else
+            new_cubes[i][j][k][f] = '.'
+          end
         end
       end
     end
@@ -74,7 +83,9 @@ count = 0
 cubes.values.each do |plane|
   plane.values.each do |row|
     row.values.each do |cube|
-      count += 1 if cube == '#'
+      cube.values.each do |foobar|
+        count += 1 if foobar == '#'
+      end
     end
   end
 end
