62339bee962ae358edf33e8e47819317837ac95f
Hanna Kalinowska <hanna.kalinowska@simplybusiness.co.uk>
2021-06-25 15:13:14 +0100

    2015 Day 5 part 2

--- 2015/05-main.rb
+++ 2015/05-main.rb
@@ -3,23 +3,18 @@
 inputs = File.read('05-input.txt').split
 
 def rule_one?(line)
-  line.split('').select {|c| %w(a e o i u).include?(c) }.length >= 3
+  line =~ /(\w\w).*\1/
 end
 
 def rule_two?(line)
-  line =~ /(\w)\1/
-end
-
-def rule_three?(line)
-  line !~ /ab|cd|pq|xy/
+  line =~ /(\w).\1/
 end
 
 nice = inputs.select do |line|
   one = rule_one?(line)
   two = rule_two?(line)
-  three = rule_three?(line)
 
-  one && two && three
+  one && two
 end
 
 puts nice.length
