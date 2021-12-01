30a0ae2776fe815fd0f52a0aa95cb84a51d7f552
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2021-12-01 07:12:17 +0000

    Day 1 part 2

--- 2021/01-main.rb
+++ 2021/01-main.rb
@@ -9,4 +9,13 @@ inputs.each do |number|
   prev_number = number
 end
 
+prev_sum = nil
+count = 0
+(0 .. inputs.length - 3).each do |start|
+  sum = inputs[start .. start + 2].sum
+
+  count += 1 if !prev_sum.nil? && sum > prev_sum
+  prev_sum = sum
+end
+
 puts count
