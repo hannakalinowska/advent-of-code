b8725b972473fbc9ba0bdf22cfd48a0fa79fbff7
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2021-12-02 19:33:15 +0000

    Day 2 part 2

--- 2021/02-main.rb
+++ 2021/02-main.rb
@@ -4,16 +4,18 @@ lines = File.read('02-input.txt').split(/\n/)
 
 horizontal = 0
 depth = 0
+aim = 0
 
 lines.each do |line|
   direction, amount = line.split(" ")
   case direction
   when "forward"
     horizontal += amount.to_i
+    depth += aim * amount.to_i
   when "down"
-    depth += amount.to_i
+    aim += amount.to_i
   when "up"
-    depth -= amount.to_i
+    aim -= amount.to_i
   end
 end
 
