15d5003927d7faa011e99bc854a3edc03f41304c
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2021-12-03 21:27:20 +0000

    Day 3 part 2

--- 2021/03-main.rb
+++ 2021/03-main.rb
@@ -1,15 +1,47 @@
 #! /usr/bin/env ruby
 
 inputs = File.read('03-input.txt').split(/\n/).map {|line| line.split("") }
-inputs = inputs.transpose
 
-gamma = ""
-epsilon = ""
+#inputs = "00100
+#11110
+#10110
+#10111
+#10101
+#01111
+#00111
+#11100
+#10000
+#11001
+#00010
+#01010".split(/\n/).map {|l| l.split ''}
 
-inputs.each do |line|
+generator = inputs.clone
+scrubber = inputs.clone
+
+i = 0
+loop do
+  line = generator.transpose[i]
   count = line.reduce({'0' => 0, '1' => 0}) {|acc, l| acc[l] += 1; acc}
-  gamma << count.max {|a,b| a.last <=> b.last }.first
-  epsilon << count.min {|a,b| a.last <=> b.last }.first
+
+  max = '1' if count['0'] == count['1']
+  max ||= count.max {|a,b| a.last <=> b.last }.first
+
+  generator = generator.select {|l| l[i] == max }
+  break if generator.size <= 1
+  i += 1
 end
 
-puts gamma.to_i(2) * epsilon.to_i(2)
+i = 0
+loop do
+  line = scrubber.transpose[i]
+  count = line.reduce({'0' => 0, '1' => 0}) {|acc, l| acc[l] += 1; acc}
+
+  min = '0' if count['0'] == count['1']
+  min ||= count.min {|a,b| a.last <=> b.last }.first
+
+  scrubber = scrubber.select {|l| l[i] == min }
+  break if scrubber.size <= 1
+  i += 1
+end
+
+puts generator.join.to_i(2) * scrubber.join.to_i(2)
