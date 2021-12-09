b05fe58763d1ffa7188dbda01d1364dca83da570
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2021-12-09 20:37:27 +0000

    Day 9 part 2

--- 2021/09-main.rb
+++ 2021/09-main.rb
@@ -18,16 +18,36 @@ def right(i, j, inputs)
   j < inputs.first.size - 1 ? inputs[i][j+1] : 99
 end
 
-sum = 0
+def low_point?(i, j, n, inputs)
+  top(i, j, inputs) > n &&
+    left(i, j, inputs) > n &&
+    bottom(i, j, inputs) > n &&
+    right(i, j, inputs) > n
+end
+
+basins = []
 inputs.each_with_index do |row, i|
   row.each_with_index do |n, j|
-    if top(i, j, inputs) > n &&
-        left(i, j, inputs) > n &&
-        bottom(i, j, inputs) > n &&
-        right(i, j, inputs) > n
-      sum = sum + 1 + n
+    if low_point?(i, j, n, inputs)
+      queue = []
+      visited = Array.new(1) { Array.new }
+      queue.push([i, j])
+      while !queue.empty?
+        a, b = queue.shift
+
+        visited[a] ||= []
+        visited[a-1] ||= []
+        visited[a+1] ||= []
+        visited[a][b] = 1
+
+        queue.push([a-1, b]) if !visited.fetch(a-1, [])[b] && top(a, b, inputs) < 9
+        queue.push([a, b-1]) if !visited.fetch(a, [])[b-1] && left(a, b, inputs) < 9
+        queue.push([a+1, b]) if !visited.fetch(a+1, [])[b] && bottom(a, b, inputs) < 9
+        queue.push([a, b+1]) if !visited.fetch(a, [])[b+1] && right(a, b, inputs) < 9
+      end
+      basins << visited.flatten.compact.size
     end
   end
 end
 
-puts sum
+puts basins.sort.reverse.take(3).reduce(&:*)
