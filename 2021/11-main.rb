68f86d018d67c05eaf40d39cab873d4d8d132ea3
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2021-12-11 14:27:11 +0000

    Day 11 part 1

---
+++ 2021/11-main.rb
@@ -0,0 +1,51 @@
+#! /usr/bin/env ruby
+
+inputs = File.read('11-input.txt').split(/\n/).map {|line| line.split('').map(&:to_i)}
+#inputs = <<-EOF
+#5483143223
+#2745854711
+#5264556173
+#6141336146
+#6357385478
+#4167524645
+#2176841721
+#6882881134
+#4846848554
+#5283751526
+#EOF
+#inputs = inputs.split(/\n/).map {|line| line.split('').map(&:to_i)}
+
+def step(inputs)
+  inputs.each_with_index do |line, i|
+    line.each_with_index do |c, j|
+      inputs[i][j] += 1
+    end
+  end
+
+  while inputs.flatten.any? {|n| n > 9 } do
+    inputs.each_with_index do |line, i|
+      line.each_with_index do |c, j|
+        if c > 9
+          inputs[i][j] = 0
+          @flashes += 1
+
+          inputs[i+1][j-1] += 1 if j > 0 && inputs[i+1][j-1].to_i > 0 rescue nil
+          inputs[i+1][j] += 1 if inputs[i+1][j].to_i > 0 rescue nil
+          inputs[i+1][j+1] += 1 if inputs[i+1][j+1].to_i > 0 rescue nil
+          inputs[i][j-1] += 1 if j > 0 && inputs[i][j-1].to_i > 0 rescue nil
+          inputs[i][j+1] += 1 if inputs[i][j+1].to_i > 0 rescue nil
+          inputs[i-1][j-1] += 1 if i > 0 && j > 0 && inputs[i-1][j-1].to_i > 0 rescue nil
+          inputs[i-1][j] += 1 if i > 0 && inputs[i-1][j].to_i > 0 rescue nil
+          inputs[i-1][j+1] += 1 if i > 0 && inputs[i-1][j+1].to_i > 0 rescue nil
+        end
+      end
+    end
+  end
+end
+
+@flashes = 0
+100.times do |i|
+  step(inputs)
+end
+
+puts @flashes
