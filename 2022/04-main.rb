b05e5076f9199f149be7b40d0e7aef6f3db37953
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2022-12-04 08:01:22 +0000

    [2022] Day 4 part 1

---
+++ 2022/04-main.rb
@@ -0,0 +1,23 @@
+#! /usr/bin/env ruby
+
+input = File.read('04-input.txt').split
+
+count = 0
+input.each do |line|
+  elf1, elf2 = line.split(',')
+
+  elf1 =~ /^(\d+)-(\d+)$/
+  elf1_start = $1.to_i
+  elf1_end = $2.to_i
+
+  elf2 =~ /^(\d+)-(\d+)$/
+  elf2_start = $1.to_i
+  elf2_end = $2.to_i
+
+  if (elf1_start <= elf2_start && elf1_end >= elf2_end) ||
+      (elf2_start <= elf1_start && elf2_end >= elf1_end)
+    count += 1
+  end
+end
+
+puts count
