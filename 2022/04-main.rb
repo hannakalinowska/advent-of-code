4cadb4b04e0b6a57d9bf8beedd5a548a5d37856f
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2022-12-04 08:10:30 +0000

    [2022] Day 4 part 2

--- 2022/04-main.rb
+++ 2022/04-main.rb
@@ -14,8 +14,10 @@ input.each do |line|
   elf2_start = $1.to_i
   elf2_end = $2.to_i
 
-  if (elf1_start <= elf2_start && elf1_end >= elf2_end) ||
-      (elf2_start <= elf1_start && elf2_end >= elf1_end)
+  if (elf1_start .. elf1_end).include?(elf2_start) ||
+      (elf1_start .. elf1_end).include?(elf2_end) ||
+      (elf2_start .. elf2_end).include?(elf1_start) ||
+      (elf2_start .. elf2_end).include?(elf1_end)
     count += 1
   end
 end
