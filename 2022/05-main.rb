19f4c8fa95a1bd5cb820441cddfbcb4396a653c8
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2022-12-05 19:56:37 +0000

    [2022] Day 5 part 2

--- 2022/05-main.rb
+++ 2022/05-main.rb
@@ -21,7 +21,7 @@ moves.each_line do |move|
   to = $3.to_i - 1
 
   crates = new_stacks[from].shift(count)
-  new_stacks[to].unshift(crates.reverse).flatten!
+  new_stacks[to].unshift(crates).flatten!
 end
 
 puts new_stacks.collect {|s| s.first}.join('').inspect
