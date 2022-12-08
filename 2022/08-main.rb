28b578143f987da32f4feecb6d54f3d0b8f0aaad
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2022-12-08 17:48:59 +0000

    [2022] Day 8 part 2

--- 2022/08-main.rb
+++ 2022/08-main.rb
@@ -10,33 +10,59 @@ input = File.read('08-input.txt')
 #EOF
 input = input.split("\n").map {|line| line.split('').map(&:to_i)}
 
-def visible_left?(tree, i, j, input)
-  line = j > 0 ? input[i][0 .. j-1] : []
-  line.all? {|t| t < tree}
+def look_left(tree, i, j, input)
+  line = j > 0 ? input[i][0 .. j-1].reverse : []
+  selected = []
+  line.each do |t|
+    selected << t
+    break if t >= tree
+  end
+  selected
 end
 
-def visible_right?(tree, i, j, input)
+def look_right(tree, i, j, input)
   line = input[i][j+1 .. -1]
-  line.all? {|t| t < tree}
+  selected = []
+  line.each do |t|
+    selected << t
+    break if t >= tree
+  end
+  selected
 end
 
-def visible_top?(tree, i, j, input)
-  line = i > 0 ? input[0 .. i-1].map {|l| l[j]} : []
-  line.all? {|t| t < tree}
+def look_up(tree, i, j, input)
+  line = i > 0 ? input[0 .. i-1].map {|l| l[j]}.reverse : []
+  selected = []
+  line.each do |t|
+    selected << t
+    break if t >= tree
+  end
+  selected
 end
 
-def visible_bottom?(tree, i, j, input)
+def look_down(tree, i, j, input)
   line = input[i+1 .. -1].map {|l| l[j]}
-  line.all? {|t| t < tree}
+  selected = []
+  line.each do |t|
+    selected << t
+    break if t >= tree
+  end
+  selected
 end
 
-count = 0
+max_scenic_score = 0
 input.each_with_index do |line, i|
   line.each_with_index do |tree, j|
-    if visible_left?(tree, i, j, input) || visible_right?(tree, i, j, input) ||
-      visible_top?(tree, i, j, input) || visible_bottom?(tree, i, j, input)
-      count += 1
+    scenic_score = [
+      look_left(tree, i, j, input).length,
+      look_right(tree, i, j, input).length,
+      look_up(tree, i, j, input).length,
+      look_down(tree, i, j, input).length,
+    ].reduce(:*)
+
+    if scenic_score > max_scenic_score
+      max_scenic_score = scenic_score
     end
   end
 end
-puts count
+puts max_scenic_score
