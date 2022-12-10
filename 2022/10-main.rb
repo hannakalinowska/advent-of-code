00ae9411b4e1449eb4dc324c93e929aff08f4b9e
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2022-12-10 07:45:10 +0000

    [2022] Day 10 part 2

--- 2022/10-main.rb
+++ 2022/10-main.rb
@@ -15,6 +15,7 @@ elapsed = 0
 instruction = nil
 value = nil
 signal_strength = 0
+column = 0
 
 loop do
   # find the next instruction or continue the previous one
@@ -23,16 +24,22 @@ loop do
     elapsed = 0
   end
 
-  # execute instruction
+  # draw pixels
+  if (register - 1 .. register + 1).include?(column)
+    print '#'
+  else
+    print '.'
+  end
+
   # check signal strength
   if tick % 40 == 20
     signal_strength += tick * register
-    puts "tick: #{tick}, register: #{register}, signal_strength: #{signal_strength}"
   end
 
   # finish cycle
   elapsed += 1
   tick += 1
+  column += 1
 
   # post-cycle actions
   case instruction
@@ -41,7 +48,11 @@ loop do
       register += value.to_i
     end
   end
+  if column == 40
+    puts
+    column = 0
+  end
 
-  break if tick >= 250 || input.empty?
+  break if input.empty?
 end
-puts signal_strength
+#puts signal_strength
