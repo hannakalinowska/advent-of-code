a9f44080ffcf15bcb7dc85a48980e6ee6c4ef18b
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2023-07-04 19:59:01 +0100

    Something about holes?

--- 2022/18-main.rb
+++ 2022/18-main.rb
@@ -18,23 +18,49 @@ input = File.read('18-input.txt')
 #EOF
 input = input.split("\n")
 
+def not_hole?(p, points)
+
+end
+
+def traverse_hole(p, points)
+
+end
+
 points = input.map do |point|
   x, y, z = point.split(',').map(&:to_i)
   [x, y, z]
 end
+holes = []
+not_holes = []
 
 total_open_sides = 0
 points.each do |x, y, z|
   sides = 6
-  neighbours = [
+
+  [
     [x-1, y, z],
     [x+1, y, z],
     [x, y-1, z],
     [x, y+1, z],
     [x, y, z-1],
     [x, y, z+1],
-  ].select {|p| points.include?(p)}
+  ].each do |p|
+    if points.include?(p)
+      # nothing - has neighbour
+    elsif holes.include?(p)
+      # nothing - holes don't count
+    elsif not_hole?(p, points)
+      # definitely an open edge
+    else
+      # discover
+      traverse_hole(p, points)
+    end
+  end
+
   open_sides = sides - neighbours.size
   total_open_sides += open_sides
 end
 puts total_open_sides
+
+#require 'pry'; binding.pry
+#puts
